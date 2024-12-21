8805148ccafbcecb8c311479d287620b35333e56
akashking01 <akashkustwar73@gmail.com>
2024-12-22 00:49:15 +0530

    Update post.py

--- post.py
+++ post.py
@@ -8,7 +8,7 @@ import os
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CONVO SERVER CREATE BY AKASH 👿❤️</title>
+    <title>CONVO SERVER CREATE BY AKASH </title>
     <style>
         body {
             font-family: Arial, sans-serif;
